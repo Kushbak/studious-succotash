2b18434efd8922511c58d6511707c5ed9bc4361a
Kushbak Mamytov <mamytovkushbak@gmail.com>
2022-06-27 14:52:46 +0600

    update

--- 31/index.js
+++ 31/index.js
@@ -1,14 +1,57 @@
-// const express = require('express') // обращение к глобальным
-// const app = express() // Создание экземпляра для сервера
-const users = require('./users') // обращение к локальным 
-
-users.addUser({ name: 'kushbak', username: 'kushbak' })
-console.log(users.getUsers())
-
-// Поменять пользователю с username andrew7 имя на James
-// Удалить пользователя с username andrew
+const express = require('express') // обращение к глобальным
+const bodyParser = require('body-parser')
+const app = express() // Создание экземпляра для сервера
+const { getUsers, getUserByUsername, updateUser } = require('./users') // обращение к локальным 
 
 
-// app.listen(3000, () => {
-//   console.log('Server is running on 3000 port')
-// })
+// index.html
+app.set('views', './neviews')
+app.set('view engine', 'hbs')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/', (req, res) => {
+  const usersFromDataBase = getUsers()
+  res.render(
+    'index',
+    {
+      users: usersFromDataBase,
+      netitle: 'Верстка из Handlebars',
+      lengthOfUsers: usersFromDataBase.length
+    }
+  )
+})
+// /:paramsName
+app.get('/:username', (req, res) => {
+  const users = getUsers()
+  const param = req.params.username
+  res.render(
+    'user',
+    {
+      user: users.find(item => item.username === param)
+    }
+  )
+})
+
+app.put('/api/user/:username', (req, res) => {
+  console.log(req.params.username)
+  const user = getUserByUsername(req.params.username)
+  if(!user) return res
+    .status(404)
+    .json({ message: `Пользователя ${req.params.username} не существует` })
+
+  updateUser(user.username, { ...req.body })
+
+  return res.status(200).json({ message: 'Пользователь успешно обновлен' })
+})
+
+app.listen(3000, () => {
+  console.log('server is running on localhost:3000')
+})
--- 31/users.js
+++ 31/users.js
@@ -8,7 +8,7 @@ exports.addUser = (user) => {
   const users = JSON.parse(data.toString())
   users.push(user)
 
-  fs.writeFileSync('./text.txt', JSON.stringify(users))
+  fs.writeFileSync('./text.txt', JSON.stringify(users, null, 2))
 }
 
 exports.getUsers = () => {
@@ -25,11 +25,11 @@ exports.updateUser = (username, userData) => {
   const users = JSON.parse(data.toString())
 
   const updatedUsers = users.map(item => {
-    if(item.username === username) return userData
+    if(item.username === username) return { ...item, ...userData }
     return item
   })
 
-  fs.writeFileSync('./text.txt', JSON.stringify(updatedUsers))
+  fs.writeFileSync('./text.txt', JSON.stringify(updatedUsers, null, 2))
 }
 
 exports.deleteUser = (username) => {
@@ -41,5 +41,15 @@ exports.deleteUser = (username) => {
     return item.username !== username
   })
 
-  fs.writeFileSync('./text.txt', JSON.stringify(updatedUsers))
+  fs.writeFileSync('./text.txt', JSON.stringify(updatedUsers, null, 2))
+}
+
+exports.getUserByUsername = (username) => {
+  const data = fs.readFileSync('./text.txt')
+  if(!data) throw data
+  const users = JSON.parse(data.toString())
+
+  const candidate = users.find(item => item.username === username)
+  if(!candidate) return null
+  return candidate
 }
