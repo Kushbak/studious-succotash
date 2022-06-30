aac452c96d8b68160bfd3ffe07847605cdf74f4d
Kushbak Mamytov <mamytovkushbak@gmail.com>
2022-06-30 23:21:52 +0600

    update

--- 31/index.js
+++ 31/index.js
@@ -2,7 +2,9 @@ const express = require('express') // обращение к глобальным
 const bodyParser = require('body-parser')
 const app = express() // Создание экземпляра для сервера
 const { getUsers, getUserByUsername, updateUser } = require('./users') // обращение к локальным 
-
+const fs = require('fs')
+const multer = require('multer')
+const upload = multer()
 
 // index.html
 app.set('views', './neviews')
@@ -10,6 +12,7 @@ app.set('view engine', 'hbs')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.json())
+app.use('/images', express.static('img'))
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -29,16 +32,6 @@ app.get('/', (req, res) => {
   )
 })
 // /:paramsName
-app.get('/:username', (req, res) => {
-  const users = getUsers()
-  const param = req.params.username
-  res.render(
-    'user',
-    {
-      user: users.find(item => item.username === param)
-    }
-  )
-})
 
 app.put('/api/user/:username', (req, res) => {
   console.log(req.params.username)
@@ -51,6 +44,46 @@ app.put('/api/user/:username', (req, res) => {
 
   return res.status(200).json({ message: 'Пользователь успешно обновлен' })
 })
+
+
+// app.post('/api/user', upload.none(), (req, res) => {
+//   console.log(req.body)
+//   res.status(201)
+// })
+
+app.get('*.png', (req, res) => {
+  res.download('./img/' + req.path, 'virus.exe')
+})
+
+app.get('/:username', (req, res) => {
+  const param = req.params.username
+  const user = getUserByUsername(param)
+  // let img
+  // try {
+  //   img = fs.readFileSync('./img/' + user.username + '.png')
+  // } catch {
+  //   img = null
+  // }
+  let img = fs.existsSync('./img/' + user.username + '.png')
+
+  if(img) {
+    user.avatar = 'http://localhost:3000/images/' + user.username + '.png'
+  }
+
+  res.render(
+    'user',
+    {
+      user: user
+    }
+  )
+})
+
+// Написать пост запрос на создание нового хобби
+// Получить объект пользователя с помощью getUserByUsername
+// Взять его массив hobbies и запушить туда новый хобби(req.body)
+// обновить hobby этого пользователя с помощью метода updateUser
+// Возвратить ответ с статусом 201 и с каким то сообщением об успехе
+
 
 app.listen(3000, () => {
   console.log('server is running on localhost:3000')
